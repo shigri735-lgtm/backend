23aa31e0d3410dc93d8cc355f8dfb92596e73efa
akbar ali <akbaroofficial041@gmail.com>
2026-04-23 01:41:23 +0500

    vercel backend setup

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+// import app from "./app.js";
+// import cloudinary from "cloudinary";
+
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// // app.listen(process.env.PORT  || 3000, () => {
+// //   console.log(`Server listening at port ${process.env.PORT}`);
+// // });
+// const PORT = process.env.PORT ;
+// app.listen(PORT, () => {
+//   console.log(`Server listening at port ${PORT}`);
+// });
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
@@ -7,10 +23,4 @@ cloudinary.v2.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// app.listen(process.env.PORT  || 3000, () => {
-//   console.log(`Server listening at port ${process.env.PORT}`);
-// });
-const PORT = process.env.PORT ;
-app.listen(PORT, () => {
-  console.log(`Server listening at port ${PORT}`);
-});
+export default app;
