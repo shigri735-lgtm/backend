d5a21cafad890ed20270067d1971284e8441470a
akbar ali <akbaroofficial041@gmail.com>
2026-04-23 12:31:44 +0500

    fix mongodb connection

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,15 +1,30 @@
+// import mongoose from "mongoose";
+
+// const dbConnection = () => {
+//   mongoose
+//     .connect(process.env.MONGO_URI)
+//     // .connect("mongodb+srv://akbaroofficial041_db_user:6o6CV6kaKAQCelju@cluster0.tfbngdq.mongodb.net/myPortfolio" )
+//     .then(() => {
+//       console.log("Connected to database!");
+//     })
+//     .catch((err) => {
+//       console.log("Some error occured while connecting to database:", err);
+//     });
+// };
+
+// export default dbConnection;
+
 import mongoose from "mongoose";
 
-const dbConnection = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    // .connect("mongodb+srv://akbaroofficial041_db_user:6o6CV6kaKAQCelju@cluster0.tfbngdq.mongodb.net/myPortfolio" )
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log("Some error occured while connecting to database:", err);
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "portfolio"
     });
+    console.log("Connected to database!");
+  } catch (error) {
+    console.log("Database connection error:", error);
+  }
 };
 
 export default dbConnection;
